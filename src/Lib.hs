77745bed46e207adaf889ec8b871997cd0a13783
Ryan Ling <ryan@outlook.com.au>
2019-01-09 01:19:22 +1100

    Reduce IO

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,22 @@
 module Lib
   ( fizzBuzz
-  , generateResults
   )
 where
 
-import           Control.Arrow                  ( (>>>) )
-import           Data.Maybe                     ( fromMaybe )
+import           Data.Maybe                     ( fromMaybe
+                                                , listToMaybe
+                                                )
 import           GHC.Natural                    ( Natural )
 import           Text.Read                      ( readMaybe )
 
-fizzBuzz :: Arguments -> IO ()
-fizzBuzz = getCount >>> generateResults >>> mapM_ putStrLn
+class Monad m => LinePrinter m
+  where printLine :: String -> m ()
+
+instance LinePrinter IO
+  where printLine = putStrLn
+
+fizzBuzz :: LinePrinter m => Arguments -> m ()
+fizzBuzz = mapM_ printLine . generateResults . readCount
 
 generateResults :: Natural -> [Result]
 generateResults count = toResult <$> [1 .. count]
@@ -24,9 +30,8 @@ toResult num | num `multipleOf` 3 && num `multipleOf` 5 = "FizzBuzz"
 defaultCount :: Natural
 defaultCount = 100
 
-getCount :: Arguments -> Natural
-getCount [arg] = fromMaybe defaultCount (readMaybe arg :: Maybe Natural)
-getCount _     = defaultCount
+readCount :: Arguments -> Natural
+readCount args = fromMaybe defaultCount $ readMaybe =<< listToMaybe args
 
 multipleOf :: Integral a => a -> a -> Bool
 multipleOf x y = x `mod` y == 0
